c2a80e940baf95247b2a72812195a91452762be7
purlimurlikas-maker <purlimurlikas@gmail.com>
2026-09-11 20:07:00 +0300

    commit all

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     let media;
 
     if (data.media_type === "image") {
-      media = `<img src="${data.url}" class="NASAAPI"/>`;
+      media = `<img src="${data.url}" style="border: 3px; border-color: #306bac; width: 12rem; height: auto;"/>`;
     } else {
-      media = `<video src="${data.url}" controls class="NASAAPI"></video>`;
+      media = `<video src="${data.url}" controls style="border: 3px; border-color: #306bac; width: 12rem; height: auto;"></video>`;
     }
 
     document.querySelector("#app").innerHTML = `
